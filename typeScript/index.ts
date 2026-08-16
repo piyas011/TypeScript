f365e2d2e4b452b4533bd0055498526ecaf5616a
Piyas Ahmed <apiyas761@gmail.com>
2026-08-16 18:31:53 +0600

    typeScript

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,3 +1,21 @@
-const userName: string = "Piyas Ahmed";
+let userName: string = "Piyas Ahmed";
+let age: number = 21;
+let isDeveloper: boolean = true;
 
-console.log(userName);
+const number: number[] = [10, 23, 34, 45, 56];
+
+const student: {
+  name: string;
+  age: number;
+  course: string;
+  isActive: boolean;
+} = {
+  name: "Piyas",
+  age: 21,
+  course: "typeScript",
+  isActive: true,
+};
+
+function add(a: number, b: number): number {
+  return a + b;
+}
