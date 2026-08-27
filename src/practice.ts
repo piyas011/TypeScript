8f0dd069cd09e128867a5ef90e980d78a4b73cab
Piyas Ahmed <apiyas761@gmail.com>
2026-08-27 14:18:41 +0600

    Practice

--- src/practice.ts
+++ src/practice.ts
@@ -417,11 +417,17 @@
     const averageSalary = totalSalary / employees.length;
     // console.log(averageSalary);
 
-    const searchById: number = 2;
-    const searchEmployee = employees.find(
-      (employee) => employee.id === searchById,
-    );
-    // console.log(searchEmployee);
+    function search(id: number) {
+      const searchEmployee = employees.find((employee) => employee.id === id);
+      return searchEmployee;
+    }
+    ``;
+    const searchEmployee = search(1);
+    // const searchById: number = 2;
+    // const searchEmployee = employees.find(
+    //   (employee) => employee.id === searchById,
+    // );
+    // // console.log(searchEmployee);
 
     const highSalaryEmployees = employees.filter(
       (employee) => employee.salary >= 20000,
@@ -437,5 +443,130 @@
       highSalaryEmployees,
     };
   };
-  console.log(employeeManagement(employees));
+  // console.log(employeeManagement(employees));
+}
+{
+  // 🔴 Task 10 — Final Challenge
+  // Student Management System
+
+  interface Student {
+    id: number;
+    name: string;
+    age: number;
+    marks: number[];
+  }
+
+  const students: Student[] = [
+    {
+      id: 1,
+      name: "Piyas",
+      age: 21,
+      marks: [80, 75, 90],
+    },
+    {
+      id: 2,
+      name: "Rahim",
+      age: 22,
+      marks: [65, 70, 68],
+    },
+    {
+      id: 3,
+      name: "Karim",
+      age: 23,
+      marks: [88, 92, 85],
+    },
+    {
+      id: 4,
+      name: "Sakib",
+      age: 20,
+      marks: [55, 60, 58],
+    },
+    {
+      id: 5,
+      name: "Nayeem",
+      age: 22,
+      marks: [95, 90, 93],
+    },
+    {
+      id: 6,
+      name: "Hasan",
+      age: 21,
+      marks: [72, 68, 75],
+    },
+    {
+      id: 7,
+      name: "Rafi",
+      age: 24,
+      marks: [45, 52, 48],
+    },
+    {
+      id: 8,
+      name: "Arman",
+      age: 22,
+      marks: [82, 78, 85],
+    },
+    {
+      id: 9,
+      name: "Tanvir",
+      age: 23,
+      marks: [60, 65, 62],
+    },
+    {
+      id: 10,
+      name: "Shuvo",
+      age: 20,
+      marks: [35, 42, 38],
+    },
+  ];
+
+  const studentManagementSystem = (studentsObject: Student[]) => {
+    // console.log(studentsObject);
+
+    //
+    const totalMark = studentsObject.map(
+      (student) => student.marks.reduce((acc, mark) => acc + mark),
+      0,
+    );
+    // console.log(totalMark);
+
+    // calculate student average marks
+    const studentAverageMark = studentsObject.map((student) => {
+      let studentMarks =
+        student.marks.reduce((acc, mark) => acc + mark) / student.marks.length;
+      return Number(studentMarks.toFixed(0));
+    });
+    // console.log(studentAverageMark);
+
+    // Grade Calculate
+    const studentGradeCalculate = studentAverageMark.map((mark) => {
+      const gradeCalculate = (mark: number): {} => {
+        if (mark >= 80) {
+          return "A+";
+        } else if (mark >= 70) {
+          return "A";
+        } else if (mark >= 60) {
+          return "A-";
+        } else if (mark >= 50) {
+          return "B";
+        } else if (mark >= 40) {
+          return "C";
+        } else if (mark >= 33) {
+          return "D";
+        }
+
+        return "F";
+      };
+      return { [mark]: gradeCalculate(mark) };
+    });
+    // console.log(studentGradeCalculate);
+
+    ///////////////////////////////////////////
+    return {
+      totalMark: totalMark,
+      studentAverageMark: studentAverageMark,
+      studentGradeCalculate: studentGradeCalculate,
+    };
+  };
+
+  console.log(studentManagementSystem(students));
 }
